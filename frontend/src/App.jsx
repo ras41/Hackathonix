324a623be3dc59f7fa11c25c704cd94a62940d32
Rishi9822 <rishiiip31@gmail.com.com>
2026-03-09 09:49:58 +0530

    Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-500">
+      Tailwind is working 🚀
+    </h1>
+  )
+}
